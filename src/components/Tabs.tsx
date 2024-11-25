dde940cc567c1a16ab711dba0decaee9f2f3e55d
kaeidev <kowindeveloper@gmail.com>
2024-11-25 21:28:44 +0900

    タブのスタイルを追加する

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -2,17 +2,39 @@ import { NavLink } from "react-router";
 
 export const Tabs = () => {
   return (
-    <div>
-      <div>
-        <NavLink to="/" end>
-          Home
-        </NavLink>
-      </div>
-      <div>
-        <NavLink to="/todo" end>
-          Todo
-        </NavLink>
-      </div>
+    <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <ul className="flex flex-wrap -mb-px">
+        <li className="me-2">
+          <NavLink
+            to="/"
+            className={({ isActive }) => {
+              const style = "inline-block p-4 border-b-2 rounded-t-lg";
+              const activeStyle = " text-blue-600 border-blue-600 active";
+              return isActive
+                ? `${style} ${activeStyle}`
+                : `${style} border-transparent hover:text-gray-600 hover:border-gray-300`;
+            }}
+            end
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="me-2">
+          <NavLink
+            to="/todo"
+            className={({ isActive }) => {
+              const style = "inline-block p-4 border-b-2 rounded-t-lg";
+              const activeStyle = " text-blue-600 border-blue-600 active";
+              return isActive
+                ? `${style} ${activeStyle}`
+                : `${style} border-transparent hover:text-gray-600 hover:border-gray-300`;
+            }}
+            end
+          >
+            Todo
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
